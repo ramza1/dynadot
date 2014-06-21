c0eec172a7fa0574d0fa8d38fc58a3c51851924c
Uchenna Okafor <ramzauchenna@gmail.com>
2014-06-21 12:50:13 +0100

    test

--- dynadot/lib/dynadot.rb
+++ dynadot/lib/dynadot.rb
@@ -1,10 +1,11 @@
 module Dynadot
  class Api3
   include HTTParty
+  if de
   def initialize
-   api_key = DYNADOT_KEY
+   @api_key = DYNADOT_KEY if defined? DYNADOT_KEY
   end
-  base_uri "https://api.dynadot.com/api3.xml?key=#{api_key}"
+  base_uri "https://api.dynadot.com/api3.xml?key=#{@api_key}"
 
   def search_domain(params)
      self.class.get("&command=search&domain0=#{params}&domain1=#{params}.net&domain2=#{params}.ng&domain3=#{params}.me")
